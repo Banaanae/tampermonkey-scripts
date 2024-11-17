a34bd9f4641c770f36f642bb6bebb6a518714b25
Banaanae <83927639+Banaanae@users.noreply.github.com>
2024-11-17 15:53:23 +1100

    Update AHK-Logo-To-Forum-Root.user.js

--- AHK-Logo-To-Forum-Root.user.js
+++ AHK-Logo-To-Forum-Root.user.js
@@ -1,16 +1,18 @@
 // ==UserScript==
 // @name         AHK Logo to forum root
 // @namespace    https://github.com/Banaanae/tampermonkey-scripts/
-// @version      v1.0.0
+// @version      1.0.1
 // @description  Replaces to logo image link from autohotkey.com to ahk forums
 // @author       Banaanae
 // @match        https://www.autohotkey.com/boards/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=autohotkey.com
+// @icon         https://autohotkey.com/favicon.ico
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    document.getElementById('logo').href = 'https://www.autohotkey.com/boards/index.php';
+    const logo = document.getElementById('logo');
+    logo.href = 'https://www.autohotkey.com/boards/index.php';
+    logo.title = 'Forum index';
 })();
